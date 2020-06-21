b58054744ab45237c5bc358c6a838a130cda862e
Ibekendu Neche <nechekendu@gmail.com>
2020-06-21 21:31:11 +0100

    how to print !st of January that is on Sunday.

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ var myDate = new Date();
 myDate;
 
 console.log("Sample Output: " + `Today is ${myDate} .`);
+
+
+//Write a Javascript to find the !st of January between 2014 and 2050
+ 
+for (let year = 2014; year <= 2050; year++) {
+    let today =  new Date(year, 0, 1);
+    var month = today.getMonth();
+    var date = today.getDate();
+    var year1 = today.getFullYear();
+    var day = today.getDay();
+
+    if (date === 1 && day === 0 && month == 0) {
+        console.log(year);
+    } else {
+        alert("error");
+    };
+};
